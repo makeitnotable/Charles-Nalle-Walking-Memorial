da6109f1bdd1473c467d6ce5feda1605e1c21e76
Michele Mele <chellymele@gmail.com>
2025-10-03 11:20:08 -0700

    animations

--- src/components/location-page/QuoteSection.jsx
+++ src/components/location-page/QuoteSection.jsx
@@ -1,57 +1,9 @@
-import { useEffect, useRef } from 'react';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { isMobile } from 'react-device-detect';
 import ArrowDown from '../ArrowDown';
 
-// Register ScrollTrigger plugin
-gsap.registerPlugin(ScrollTrigger);
-
 export default function QuoteSection({ data }) {
-    const backgroundRef = useRef(null);
-
-    useEffect(() => {
-        const element = backgroundRef.current;
-
-        if (element) {
-            // Create the scroll trigger animation
-            gsap.fromTo(element,
-                {
-                    scale: 1,
-                },
-                {
-                    scale: 1.2,
-                    duration: 1,
-                    ease: "power2.out",
-                    scrollTrigger: {
-                        trigger: element,
-                        start: "top bottom",
-                        end: "bottom top",
-                        scrub: 1, // Makes animation smooth and tied to scroll position
-                    }
-                }
-            );
-        }
-
-        // Cleanup function
-        return () => {
-            ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-        };
-    }, []);
-
     return (
         <div className="h-screen">
-            <div className="relative h-full p-4 text-left  overflow-hidden">
-                <div
-                    ref={backgroundRef}
-                    className="absolute inset-0 z-0 will-change-transform h-full"
-                    style={{
-                        backgroundImage: `linear-gradient(rgba(16, 10, 6, 0.8), rgba(16, 10, 6, 0.8)), url('${isMobile ? data.backgroundImage.vertical : data.backgroundImage.horizontal}')`,
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        backgroundRepeat: 'no-repeat',
-                    }}
-                />
+            <div className="relative h-full p-4 text-left">
                 <div className="flex flex-col justify-center items-center h-full relative z-10">
                     <div className="max-w-md flex flex-col items-center">
                         <div className="h-[100px] bg-primary-12 w-[1px] mb-10" /></div>
