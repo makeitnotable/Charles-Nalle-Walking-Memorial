e0b7185275b6b1bb62e46c45d5c1bb72d658877c
Giuseppe Station <giuseppe@stationsciences.com>
2025-10-25 10:44:34 -0600

    fix: hosting

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
+  base: "/Charles-Nalle-Walking-Memorial/",
   server: {
     historyApiFallback: true, // Ensures client-side routing works in development
     allowedHosts: ['3b49-73-169-38-78.ngrok-free.app'],
