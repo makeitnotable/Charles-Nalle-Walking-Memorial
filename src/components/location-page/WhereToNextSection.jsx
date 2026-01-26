ef855aa694d8551e7fc665efa1cb89dc895c5430
whoislewys <whoislewys@gmail.com>
2026-01-26 12:10:10 -0500

    wheretonext section internal vertical spacing

--- src/components/location-page/WhereToNextSection.jsx
+++ src/components/location-page/WhereToNextSection.jsx
@@ -17,14 +17,14 @@ export default function WhereToNextSection({ currentChapter }) {
   };
   return (
     <div className='flex flex-col gap-y-8 md:gap-y-12 md:gap-y-16 px-4 pt-4 md:py-4 lg:py-8 mt-8 md:mt-0'>
-      <div className='space-y-2'>
+      <div className='flex flex-col gap-y-2'>
         <h3 className='text-[#F6F3EE] text-[2.625rem] leading-[2.125rem] tracking-[-0.09375rem] md:text-[3.28125rem] md:leading-[2.65625rem] md:tracking-[-0.11719rem] lg:text-[3.9375rem] lg:leading-[3.1875rem] lg:tracking-[-0.14063rem] font-["Martel_Sans"] font-semibold pt-1 lg:pt-0.5 mb-0'>
           {currentChapter.whereToNext.title.split(' ')[0]}
           <br />
           {currentChapter.whereToNext.title.split(' ').slice(1).join(' ')}
         </h3>
         {/* Show progress indicator above mapbox on mobile */}
-        <div className="block md:hidden">
+        <div className="block py-4 md:hidden">
           <ProgressIndicator className='text-[#F6F3EE] ml-2 my-0'>{currentChapter.whereToNext.number}</ProgressIndicator>
         </div>
       </div>
@@ -42,7 +42,7 @@ export default function WhereToNextSection({ currentChapter }) {
         <ProgressIndicator className='text-[#F6F3EE] ml-2 my-0'>{currentChapter.whereToNext.number}</ProgressIndicator>
       </div>
 
-      <div className='flex w-full flex-row justify-center items-center p-8 mt-10 md:mt-12'>
+      <div className='flex w-full flex-row justify-center items-center my-8 md:my-0'>
         <Button variant='outline' className='' onClick={handleGetDirections}>
           Get Directions
         </Button>
