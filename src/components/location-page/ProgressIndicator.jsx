9451c5ecc15669a7e393b0fbe65c01b79d016c52
whoislewys <whoislewys@gmail.com>
2026-01-26 11:20:11 -0500

    vertical spacing inside moral message section

--- src/components/location-page/ProgressIndicator.jsx
+++ src/components/location-page/ProgressIndicator.jsx
@@ -1,6 +1,6 @@
-export default function ProgressIndicator({ children, className }) {
+export default function ProgressIndicator({ children, className, ref }) {
   return (
-    <p className={`text-[0.75rem] md:text-[0.9375rem] lg:text-[1.125rem] leading-[1.125rem] md:leading-[1.875rem] lg:leading-[1.6875rem] font-[500] font-poppins ${className}`}>
+    <p ref={ref} className={`text-[0.75rem] md:text-[0.9375rem] lg:text-[1.125rem] leading-[1.125rem] md:leading-[1.875rem] lg:leading-[1.6875rem] font-[500] font-poppins ${className}`}>
       {children}
     </p>
   );
